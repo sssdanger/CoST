8e97062338c0ffc5d7b73afaab554b460735cca9
sssdanger <78289876+sssdanger@users.noreply.github.com>
2024-08-09 13:53:21 +0800

    update

--- utils/dataset.py
+++ utils/dataset.py
@@ -3,10 +3,7 @@ import numpy as np
 import torch
 import random
 from torch.utils.data import Dataset
-from utils.utils import get_neighbor_finder
 
-
-# from torch_geometric.data import Data
 class Data:
     def __init__(self, sources, destinations, time_spans, current_time, parrent_time, edge_idxs, labels, node_features,
                  adj_list, id, node_depth, node_child, edge_index, BU_edge_index, x):
@@ -28,7 +25,6 @@ class Data:
         self.edge_index = edge_index
         self.BU_edge_index = BU_edge_index
         self.x = x
-
 def find_depth(sources, destinations, node_size):
     node_depth = []
     node_child = []
@@ -133,129 +129,14 @@ class UdGraphDataset(Dataset):
                          x=torch.tensor(data['x'][:end],dtype=torch.float32))
         return full_data
 
-
-class UdTestGraphDataset(Dataset):
-    def __init__(self, fold_x, droprate=0.2, subrate=0.2,
-                 data_path=None):
-        self.fold_x = fold_x
-        self.data_path = data_path
-        self.droprate = droprate
-        self.subrate = subrate
-
-    def __len__(self):
-        return len(self.fold_x)
-
-    def __getitem__(self, index):
-        id = self.fold_x[index]
-        data = np.load(os.path.join(self.data_path, id + ".npz"), allow_pickle=True)
-
-        edgeindex = data['edgeindex']
-        edge_id = np.array(data['edg_index'])[:-1]
-        time_spa = np.array(data['time_spa'][1:])
-        cur_time = np.array(data['cur_time'])[1:]
-        par_time = np.array(data['par_time'])[1:]
-        node_features = np.array(data['x'])
-
-        subset_idxs = np.array(range(int(self.subrate * len(cur_time))))
-        if len(subset_idxs) == 0:
-            subset_idxs = np.array([0])
-        edgeindex = np.array([edgeindex[0][subset_idxs], edgeindex[1][subset_idxs]])
-        edge_id = edge_id[subset_idxs]
-        time_spa = time_spa[subset_idxs]
-        cur_time = cur_time[subset_idxs]
-        par_time = par_time[subset_idxs]
-
-
-        empty = np.zeros(node_features.shape[1])[np.newaxis, :]
-        node_features = np.vstack([empty, node_features])
-        label = int(data['y'])
-
-        if self.droprate > 0:
-            # dropout part
-            length = len(edgeindex[0])
-            poslist = random.sample(range(length), int(length * (1 - self.droprate)))
-            poslist = sorted(poslist)
-            edge_id = edge_id[poslist]
-            time_spa = time_spa[poslist]
-            cur_time = cur_time[poslist]
-            par_time = par_time[poslist]
-
-            row = list(edgeindex[0][poslist])
-            col = list(edgeindex[1][poslist])
-            burow = list(edgeindex[1][poslist])
-            bucol = list(edgeindex[0][poslist])
-            sources = edgeindex[0][poslist] + 1
-            destinations = edgeindex[1][poslist] + 1
-            #
-        else:
-            row = list(edgeindex[0])
-            col = list(edgeindex[1])
-            burow = list(edgeindex[1])
-            bucol = list(edgeindex[0])
-            sources = edgeindex[0] + 1
-            destinations = edgeindex[1] + 1
-
-        row.extend(burow)
-        col.extend(bucol)
-        new_edgeindex = np.array([row, col]).reshape(2, -1)
-
-        full_data = Data(sources, destinations, time_spa, cur_time, par_time, edge_id, label, node_features,
-                         adj_list=new_edgeindex, id=id)
-        return full_data
-
-################################### load tree#####################################
-def loadTree(dataname):
-    data_eid = np.load('./labels.npy', allow_pickle=True)
-    treeDic = []
-    for eid in data_eid.item():
-        treeDic.append(eid)
-    return treeDic
-
-
 ################################# load data ###################################
-
-def loadUdData(fold_x_train, fold_x_test):  # 加载的是双向 data
-    data_path = './Twitter_15_and_16'
-    print("loading train set")
-    traindata_list = UdGraphDataset(fold_x_train, data_path=data_path)
-    testdata_list = UdGraphDataset(fold_x_test, data_path=data_path)
-    print("train no:", len(traindata_list))
-    print("test no:", len(testdata_list))
-    return traindata_list, testdata_list
-
-
-def loadFNNData(fold_x_train, fold_x_test):
-    # data_path = './fakenewsnet'
-    # data_path = './PHEME_new/'
+def loadData(fold_x_train, fold_x_test):
     # data_path = './Weibo_finished/'
-    # data_path = './Twitter_finished/'
     # data_path = './Poli_finished/'
     data_path = './Gossi_finished/'
     print("loading train set")
     traindata_list = UdGraphDataset(fold_x_train, data_path=data_path, droprate=0.0)
-    # valdata_list = UdGraphDataset(fold_x_val, data_path=data_path, droprate=0.0)
     testdata_list = UdGraphDataset(fold_x_test, data_path=data_path, droprate=0.0)
-    print("train no:", len(traindata_list))
-    # print("val no:", len(valdata_list))
-    print("test no:", len(testdata_list))
-    return traindata_list, testdata_list
-
-
-def loadPHEMEData(fold_x_train, fold_x_test):
-    data_path = './PHEME/'
-    print("loading PHEME dataset")
-    traindata_list = UdGraphDataset(fold_x_train, data_path=data_path, droprate=0.0)
-    testdata_list = UdGraphDataset(fold_x_test, data_path=data_path, droprate=0.0)
-    print('train no: ', len(traindata_list))
-    print('test no: ', len(testdata_list))
-    return traindata_list, testdata_list
-
-
-def loadFNNTestData(fold_x_train, fold_x_test, subrate=0.2):
-    data_path = './fakenewsnet'
-    print("loading train set")
-    traindata_list = UdTestGraphDataset(fold_x_train, subrate=subrate, data_path=data_path, droprate=0.0)
-    testdata_list = UdTestGraphDataset(fold_x_test, subrate=subrate, data_path=data_path, droprate=0.0)
     print("train no:", len(traindata_list))
     print("test no:", len(testdata_list))
     return traindata_list, testdata_list
--- utils/utils.py
+++ utils/utils.py
@@ -17,77 +17,6 @@ class MergeLayer(torch.nn.Module):
     h = self.act(self.fc1(x))
     return self.fc2(h)
 
-
-class MLP(torch.nn.Module):
-  def __init__(self, dim, drop=0.3):
-    super().__init__()
-    self.fc_1 = torch.nn.Linear(dim, 80)
-    self.fc_2 = torch.nn.Linear(80, 10)
-    self.fc_3 = torch.nn.Linear(10, 1)
-    self.act = torch.nn.ReLU()
-    self.dropout = torch.nn.Dropout(p=drop, inplace=False)
-
-  def forward(self, x):
-    x = self.act(self.fc_1(x))
-    x = self.dropout(x)
-    x = self.act(self.fc_2(x))
-    x = self.dropout(x)
-    return self.fc_3(x).squeeze(dim=1)
-
-
-class EarlyStopMonitor(object):
-  def __init__(self, max_round=3, higher_better=True, tolerance=1e-10):
-    self.max_round = max_round
-    self.num_round = 0
-
-    self.epoch_count = 0
-    self.best_epoch = 0
-
-    self.last_best = None
-    self.higher_better = higher_better
-    self.tolerance = tolerance
-
-  def early_stop_check(self, curr_val):
-    if not self.higher_better:
-      curr_val *= -1
-    if self.last_best is None:
-      self.last_best = curr_val
-    elif (curr_val - self.last_best) / np.abs(self.last_best) > self.tolerance:
-      self.last_best = curr_val
-      self.num_round = 0
-      self.best_epoch = self.epoch_count
-    else:
-      self.num_round += 1
-
-    self.epoch_count += 1
-
-    return self.num_round >= self.max_round
-
-
-class RandEdgeSampler(object):
-  def __init__(self, src_list, dst_list, seed=None):
-    self.seed = None
-    self.src_list = np.unique(src_list)
-    self.dst_list = np.unique(dst_list)
-  
-    if seed is not None:
-      self.seed = seed
-      self.random_state = np.random.RandomState(self.seed)
-
-  def sample(self, size):
-    if self.seed is None:
-      src_index = np.random.randint(0, len(self.src_list), size)
-      dst_index = np.random.randint(0, len(self.dst_list), size)
-    else:
-
-      src_index = self.random_state.randint(0, len(self.src_list), size)
-      dst_index = self.random_state.randint(0, len(self.dst_list), size)
-    return self.src_list[src_index], self.dst_list[dst_index]
-
-  def reset_random_state(self):
-    self.random_state = np.random.RandomState(self.seed)
-
-
 def get_neighbor_finder(data, uniform, max_node_idx=None):
   max_node_idx = max(data.sources.max(), data.destinations.max()) if max_node_idx is None else max_node_idx
 
@@ -110,8 +39,7 @@ class NeighborFinder:
     num = 0
 
     for neighbors in adj_list:
-      # Neighbors is a list of tuples (neighbor, edge_idx, timestamp)
-      # We sort the list based on timestamp     
+
       sorted_neighhbors = sorted(neighbors, key=lambda x: x[2])
       num+=1
       self.node_to_neighbors.append(np.array([x[0] for x in sorted_neighhbors]))
@@ -125,27 +53,13 @@ class NeighborFinder:
       self.random_state = np.random.RandomState(self.seed)
 
   def find_before(self, src_idx, cut_time):
-    """
-    Extracts all the interactions happening before cut_time for user src_idx in the overall interaction graph. The returned interactions are sorted by time.
-
-    Returns 3 lists: neighbors, edge_idxs, timestamps
-
-    """
 
     i = np.searchsorted(self.node_to_edge_timestamps[src_idx], cut_time + 0.001)
 
     return self.node_to_neighbors[src_idx][:i], self.node_to_edge_idxs[src_idx][:i], self.node_to_edge_timestamps[src_idx][:i], self.node_to_edge_timespans[src_idx][:i]
 
   def get_temporal_neighbor(self, source_nodes, timestamps, n_neighbors=20):
-    """
-    Given a list of users ids and relative cut times, extracts a sampled temporal neighborhood of each user in the list.
 
-    Params
-    ------
-    src_idx_l: List[int]
-    cut_time_l: List[float],
-    num_neighbors: int
-    """
     assert (len(source_nodes) == len(timestamps))
   
     tmp_n_neighbors = n_neighbors if n_neighbors > 0 else 1
